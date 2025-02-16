9518d5b7f4542d93acbfbcc3d97df046019db5b9
Malfrador <malfrador@gmail.com>
2025-02-16 22:20:42 +0100

    Fix data mappings for players

--- src/main/java/net/worldseed/util/DataMappings.java
+++ src/main/java/net/worldseed/util/DataMappings.java
@@ -1,6 +1,7 @@
 package net.worldseed.util;
 
 import com.mojang.authlib.GameProfile;
+import net.minecraft.commands.arguments.MessageArgument;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
@@ -20,10 +21,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 public class DataMappings {
 
@@ -62,12 +60,36 @@ public class DataMappings {
                 }
             }
             DATA_ACCESSOR_MAPPINGS.put(entity.getClass(), dataAccessors);
-
         });
         // Special case for players
         try {
             Player player = new ServerPlayer(MinecraftServer.getServer(), (ServerLevel) level, new GameProfile(UUID.randomUUID(), "MappingsGenerator"), ClientInformation.createDefault());
             ENTITY_DATA_MAPPINGS.put(EntityType.PLAYER, player);
+            Map<String, EntityDataAccessor<?>> dataAccessors = new HashMap<>();
+            // its Player class, not the ServerPlayer class
+            for (Field declaredField : player.getClass().getSuperclass().getDeclaredFields()) {
+                if (!declaredField.getName().startsWith("DATA_")) {
+                    continue;
+                }
+                try {
+                    declaredField.setAccessible(true);
+                    if (declaredField.getType() != EntityDataAccessor.class) {
+                        continue;
+                    }
+                    EntityDataAccessor<?> accessor = (EntityDataAccessor<?>) declaredField.get(player);
+                    dataAccessors.put(declaredField.getName(), accessor);
+                    if (accessor != null) {
+                        ModelEngine.getProvider().getLogger().info("[Models] Found data accessor " + declaredField.getName() + " for  Player");
+                    } else {
+                        ModelEngine.getProvider().getLogger().warning("[Models] Found null data accessor " + declaredField.getName() + " for Player");
+                    }
+                } catch (IllegalAccessException e) {
+                    ModelEngine.getProvider().getLogger().warning("[Models] Failed to access data accessor field " + declaredField.getName() + " for entity Player");
+                    e.printStackTrace();
+                }
+            }
+            DATA_ACCESSOR_MAPPINGS.put(player.getClass(), dataAccessors);
+            ModelEngine.getProvider().getLogger().info("[Models] Found data accessor for Player base: " + dataAccessors.size());
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -156,11 +178,17 @@ public class DataMappings {
         Entity entity = ENTITY_DATA_MAPPINGS.get(type);
         SynchedEntityData.Builder dataBuilder = new SynchedEntityData.Builder(entity);
         try {
-            Method defineSynchedData = entity.getClass().getDeclaredMethod("defineSynchedData", SynchedEntityData.Builder.class);
-            defineSynchedData.setAccessible(true);
-            defineSynchedData.invoke(entity, dataBuilder);
+            if (entity instanceof Player player) { // We need to use the superclass for players cause of ServerPlayer
+                Method defineSynchedData = entity.getClass().getSuperclass().getDeclaredMethod("defineSynchedData", SynchedEntityData.Builder.class);
+                defineSynchedData.setAccessible(true);
+                defineSynchedData.invoke(entity, dataBuilder);
+            } else {
+                Method defineSynchedData = entity.getClass().getDeclaredMethod("defineSynchedData", SynchedEntityData.Builder.class);
+                defineSynchedData.setAccessible(true);
+                defineSynchedData.invoke(entity, dataBuilder);
+            }
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            ModelEngine.getProvider().getLogger().warning("[Models] Failed to define synched data for " + type.getDescriptionId() + ": " + e.getMessage());
+            ModelEngine.getProvider().getLogger().warning("Failed to define synched data for " + type.getDescriptionId() + ": " + e.getMessage());
         }
         // Shared data
         dataBuilder.define(getAccessor(Entity.class, "DATA_SHARED_FLAGS_ID"), (byte) 0);
@@ -176,7 +204,6 @@ public class DataMappings {
     }
 
     public static EntityDataAccessor getAccessor(Class<? extends Entity> clazz, String dataAccessor) {
-        ModelEngine.getProvider().getLogger().info("[Models] Looking for data accessor " + dataAccessor + " in class " + clazz.getName());
         if (!DATA_ACCESSOR_MAPPINGS.containsKey(clazz)) {
             ModelEngine.getProvider().getLogger().warning("[Models] No data accessors found for class " + clazz.getName());
             return null;
@@ -184,7 +211,8 @@ public class DataMappings {
         if (!DATA_ACCESSOR_MAPPINGS.get(clazz).containsKey(dataAccessor)) {
             ModelEngine.getProvider().getLogger().warning("[Models] No data accessor found for " + dataAccessor + " in class " + clazz.getName());
             // Print all we have for now
-            DATA_ACCESSOR_MAPPINGS.get(clazz).forEach((key, value) -> ModelEngine.getProvider().getLogger().info("[Models] Data accessor " + key + " for " + clazz.getName()));
+            ModelEngine.getProvider().getLogger().info("[Models] Data accessors for " + clazz.getName() + ":");
+            DATA_ACCESSOR_MAPPINGS.get(clazz).forEach((key, value) -> ModelEngine.getProvider().getLogger().info("- " + key));
             return null;
         }
         return DATA_ACCESSOR_MAPPINGS.get(clazz).get(dataAccessor);
